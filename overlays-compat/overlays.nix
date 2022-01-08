2b982894b3adb96ad1a1741255bbc6faf984a05f
Pavel Král <pavel.kral@gmail.com>
2022-01-08 21:58:23 +0100

    Link overlays.nix to /etc/static/nixos

--- overlays-compat/overlays.nix
+++ overlays-compat/overlays.nix
@@ -1,7 +1,1 @@
-self: super:
-with super.lib;
-let
-  eval = import <nixpkgs/nixos/lib/eval-config.nix>;
-  paths =
-    (eval { modules = [ (import <nixos-config>) ]; }).config.nixpkgs.overlays;
-in foldl' (flip extends) (_: super) paths self
+/etc/static/nixos/overlays-compat/overlays.nix
